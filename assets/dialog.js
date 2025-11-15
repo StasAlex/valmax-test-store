cb121e84a9e6cc45e86025ce4ba07b787117092a
StasAlex <salexandronets@corp.web4pro.net>
2025-11-15 18:51:39 +0200

    Fix first screen

--- assets/dialog.js
+++ assets/dialog.js
@@ -56,6 +56,10 @@ export class DialogComponent extends Component {
       document.body.style.width = '100%';
       document.body.style.position = 'fixed';
       document.body.style.top = `-${scrollY}px`;
+      if(dialog.classList.contains('search-modal__content')) {
+          console.log('search-modal__content');
+          document.body.classList.add('search-modal-opened')
+      }
 
       dialog.showModal();
       this.dispatchEvent(new DialogOpenEvent());
