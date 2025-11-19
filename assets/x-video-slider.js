419587910d7551519b50bde78eb4947bf3005f78
StasAlex <salexandronets@corp.web4pro.net>
2025-11-19 22:13:29 +0200

    Fix all styles

--- assets/x-video-slider.js
+++ assets/x-video-slider.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
     new Swiper(".x-video-swiper", {
         loop: true,
+        autoplay: {
+            enabled: true,
+            delay: 5000,
+        },
         slidesperView: 1,
         navigation: {
             nextEl: ".swiper-button-next",
